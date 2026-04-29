cacaf65839411d015ac86f6c750bc128dbdaad55
Serhii Shvaitser <sergiyshvaitser@gmail.com>
2026-04-29 20:57:54 +0300

    Pages cleanup

---
+++ apps/web/src/pages/tables/index.tsx
@@ -0,0 +1,10 @@
+import { TablesGrid } from "@/widgets/tables-grid";
+
+export default function TablesPage() {
+  return (
+    <div>
+      <p>Hello, tables</p>
+      <TablesGrid />
+    </div>
+  );
+}
