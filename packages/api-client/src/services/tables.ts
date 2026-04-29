775e023cf46ea0f4025ade948370bcd4dcf277d5
Serhii Shvaitser <sergiyshvaitser@gmail.com>
2026-04-29 21:40:40 +0300

    Add basic table service

---
+++ packages/api-client/src/services/tables.ts
@@ -0,0 +1,12 @@
+import { type AxiosInstance } from "axios";
+import { TablesResponseSchema, type TablesResponse } from "@repo/types";
+
+export const createTablesService = (client: AxiosInstance) => ({
+  getTables: async (): Promise<TablesResponse> => {
+    const response = await client.get("/tables");
+
+    // TODO: Error handling implementation
+
+    return TablesResponseSchema.parse(response.data);
+  },
+});
