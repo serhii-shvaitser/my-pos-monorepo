a92fb3658bc8baeee9c3a92025990149d4a55394
Serhii Shvaitser <sergiyshvaitser@gmail.com>
2026-05-07 12:52:12 +0300

    Move tanstack router config to the app layer

---
+++ apps/pos-web/src/app/router.ts
@@ -0,0 +1,25 @@
+import { createRouter } from "@tanstack/react-router";
+import { routeTree } from "../routeTree.gen";
+import { QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      retry: false,
+    },
+  },
+});
+
+export const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
