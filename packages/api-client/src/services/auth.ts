aa83ba8dccd1b080aa6b20cda877910dacad1508
Serhii Shvaitser <sergiyshvaitser@gmail.com>
2026-05-07 15:10:53 +0300

    Add auth service  methods

--- packages/api-client/src/services/auth.ts
+++ packages/api-client/src/services/auth.ts
@@ -1,13 +1,47 @@
 import { type AxiosInstance } from "axios";
-import { type PinLoginRequest, type AuthResponse } from "@repo/types";
+import { type LoginCredentials, type SessionData } from "@repo/types";
 
 export const createAuthService = (client: AxiosInstance) => ({
-  loginWithPin: async (data: PinLoginRequest) => {
-    const response = await client.post<AuthResponse>("/auth/pin", data);
-    return response.data;
+  login: async (credentials: LoginCredentials) => {
+    const { data } = await client.post<SessionData>("/auth/login", credentials);
+    return data;
   },
-  // verifyToken: async () => {
-  //   const { data } = await client.get("/auth/verify");
-  //   return data;
-  // },
+  refreshToken: async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:3001/api/v1/auth/refresh",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Refresh failed with status: " + response.status);
+      }
+      const data = await response.json();
+      return data.accessToken;
+    } catch (error) {
+      console.error("AuthApi.refreshToken error:", error);
+      throw error;
+    }
+  },
+  logout: async () => {
+    await client.post("/auth/logout");
+  },
+  test: async () => {
+    try {
+      const { data } = await client.get("/auth/test-protected", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return data;
+    } catch (error) {
+      throw new Error("test failed");
+    }
+  },
 });
