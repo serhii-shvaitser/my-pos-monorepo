fc802a9b2a492130bce299dad8f263be8d457ed7
Serhii Shvaitser <sergiyshvaitser@gmail.com>
2026-05-07 13:28:05 +0300

    Test button for the  protected api call check

--- apps/pos-web/src/pages/tables/index.tsx
+++ apps/pos-web/src/pages/tables/index.tsx
@@ -1,9 +1,19 @@
 import { TablesGrid } from "@/widgets/tables-grid";
+import { authApi } from "@/shared/lib/api";
 
 export default function TablesPage() {
+  const handleTestRequest = async () => {
+    try {
+      const data = await authApi.test();
+      console.log("Fetch: Success!", data);
+    } catch (err) {
+      console.error("Fetch: Network error", err);
+    }
+  };
   return (
     <div>
       <p>Hello, tables</p>
+      <button onClick={handleTestRequest}>Check protected api call</button>
       <TablesGrid />
     </div>
   );
