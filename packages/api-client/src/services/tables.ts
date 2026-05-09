7e75b35604252211f35d570c5db56cb4e593d02f
Serhii Shvaitser <sergiyshvaitser@gmail.com>
2026-05-09 12:59:47 +0300

    Improve getTables method; add updateTables method

--- packages/api-client/src/services/tables.ts
+++ packages/api-client/src/services/tables.ts
@@ -1,12 +1,16 @@
 import { type AxiosInstance } from "axios";
-import { TablesResponseSchema, type TablesResponse } from "@repo/types";
+import {
+  TablesResponseSchema,
+  type TablesResponse,
+  type Table,
+} from "@repo/types";
 
 export const createTablesService = (client: AxiosInstance) => ({
   getTables: async (): Promise<TablesResponse> => {
-    // const response = await client.get("/tables");
-
-    // TODO: Error handling implementation
-
-    return TablesResponseSchema.parse({ tables: [] });
+    const { data } = await client.get("/tables");
+    return TablesResponseSchema.parse(data);
+  },
+  updateTables: async (tableId: string, data: Partial<Table>) => {
+    await client.patch(`/tables/${tableId}`, data);
   },
 });
