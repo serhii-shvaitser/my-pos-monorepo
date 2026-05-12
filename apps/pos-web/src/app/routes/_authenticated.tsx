f4407662cd2a9248948d8b3e19dbdf6a469531bc
Serhii Shvaitser <sergiyshvaitser@gmail.com>
2026-05-12 21:14:26 +0300

    Update main layout

--- apps/pos-web/src/app/routes/_authenticated.tsx
+++ apps/pos-web/src/app/routes/_authenticated.tsx
@@ -1,12 +1,14 @@
-import { useSession } from "@/entities/session";
-import { Button } from "@/shared/ui/button";
-import {
-  createFileRoute,
-  Outlet,
-  Link,
-  redirect,
-} from "@tanstack/react-router";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import { useSessionStore } from "@/entities/session";
+import {
+  SidebarProvider,
+  SidebarTrigger,
+  SidebarInset,
+} from "@/shared/ui/sidebar";
+import { Button } from "@/shared/ui/button";
+import { Separator } from "@/shared/ui/separator";
+
+import { AppSidebar } from "@/widgets/sidebar";
 
 import { authApi } from "@/shared/lib/api";
 
@@ -32,26 +34,46 @@ export const Route = createFileRoute("/_authenticated")({
 });
 
 function RouteComponent() {
-  const { logout } = useSession();
   return (
-    <div className="flex h-screen">
-      <aside className="w-64 border-r bg-muted/40 p-4">
-        <nav className="flex flex-col gap-2">
-          {/* <Link to="/" className="p-2 hover:bg-accent rounded">
-            Головна
-          </Link> */}
-          <Link to="/tables" className="p-2 hover:bg-accent rounded">
-            Зали
-          </Link>
-          <Link to="/orders" className="p-2 hover:bg-accent rounded">
-            Замовлення
-          </Link>
-          <Button onClick={logout}>Logout</Button>
-        </nav>
-      </aside>
-      <main className="flex-1 p-6">
-        <Outlet />
-      </main>
-    </div>
+    <SidebarProvider defaultOpen={false}>
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+          <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mx-2 data-[orientation=vertical]:h-4"
+            />
+            <h1 className="text-base font-medium">Tables</h1>
+            <div className="ml-auto flex items-center gap-2">
+              <Button
+                variant="ghost"
+                asChild
+                size="sm"
+                className="hidden sm:flex"
+              >
+                <a
+                  href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="dark:text-foreground"
+                >
+                  Кіріній
+                </a>
+              </Button>
+            </div>
+          </div>
+        </header>
+        <main className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 px-4 md:gap-6 md:py-6">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+        <footer></footer>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
