6b8a3876e35b10d1a6e834cca0edd736bdea4636
Serhii Shvaitser <sergiyshvaitser@gmail.com>
2026-05-12 21:09:49 +0300

    Add LogoutButton feature

---
+++ apps/pos-web/src/features/logout-button/ui/LogoutButton.tsx
@@ -0,0 +1,18 @@
+import { useSession } from "@/entities/session";
+
+import { SidebarMenuButton } from "@/shared/ui/sidebar";
+
+import { LogOut } from "lucide-react";
+
+export function LogoutButton() {
+  const { logout } = useSession();
+  return (
+    <SidebarMenuButton
+      onClick={logout}
+      className="cursor-pointer bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground/90 flex items-center gap-2"
+    >
+      <LogOut className="size-4" />
+      <span>Logout</span>
+    </SidebarMenuButton>
+  );
+}
