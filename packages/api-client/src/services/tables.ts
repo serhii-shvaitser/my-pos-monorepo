d2cde41c6d9db531171c0845fb2dd0583b153005
Serhii Shvaitser <sergiyshvaitser@gmail.com>
2026-05-17 13:51:20 +0300

    Add response type, remove unnecessary parsing

--- packages/api-client/src/services/tables.ts
+++ packages/api-client/src/services/tables.ts
@@ -1,14 +1,10 @@
 import { type AxiosInstance } from "axios";
-import {
-  TablesResponseSchema,
-  type TablesResponse,
-  type Table,
-} from "@repo/types";
+import { type TablesResponse, type Table } from "@repo/types";
 
 export const createTablesService = (client: AxiosInstance) => ({
   getTables: async (): Promise<TablesResponse> => {
-    const { data } = await client.get("/tables");
-    return TablesResponseSchema.parse(data);
+    const { data } = await client.get<TablesResponse>("/tables");
+    return data;
   },
   updateTables: async (tableId: string, data: Partial<Table>) => {
     await client.patch(`/tables/${tableId}`, data);
