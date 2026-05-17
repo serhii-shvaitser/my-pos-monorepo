8e60d29e5d19293686b6350fb27ba0c887f9a406
Serhii Shvaitser <sergiyshvaitser@gmail.com>
2026-05-17 14:41:38 +0300

    Order page refactoring

---
+++ apps/pos-web/src/pages/order/index.tsx
@@ -0,0 +1,14 @@
+import { useParams } from "@tanstack/react-router";
+
+import { ActiveOrder } from "@/widgets/active-order";
+import { PosMenu } from "@/widgets/menu";
+
+export function OrderPage() {
+  const { tableId } = useParams({ from: "/_authenticated/tables/$tableId" });
+  return (
+    <div className="flex gap-2 items-stretch flex-1">
+      <ActiveOrder tableId={tableId} />
+      <PosMenu />
+    </div>
+  );
+}
