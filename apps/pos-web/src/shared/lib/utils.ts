6829580c17eb8a33391aac8e7915dd27f103e12f
Serhii Shvaitser <sergiyshvaitser@gmail.com>
2026-05-20 20:19:28 +0300

    Add formatPrice utility

--- apps/pos-web/src/shared/lib/utils.ts
+++ apps/pos-web/src/shared/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatPrice = (cents: number) => {
+  return new Intl.NumberFormat("uk-UA", {
+    style: "currency",
+    currency: "UAH",
+  }).format(cents / 100);
+};
