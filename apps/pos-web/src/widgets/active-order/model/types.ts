cd221dc0df8d1bbcbaccfa7b64c85986523ebac0
Serhii Shvaitser <sergiyshvaitser@gmail.com>
2026-06-02 19:44:21 +0300

    feat(order-page): implement table order functionality and enhance ActiveOrder component

---
+++ apps/pos-web/src/widgets/active-order/model/types.ts
@@ -0,0 +1,3 @@
+export interface ActiveOrderProps {
+  tableId: string;
+}
--- apps/pos-web/src/widgets/active-order/ui/ActiveOrder.tsx
+++ apps/pos-web/src/widgets/active-order/ui/ActiveOrder.tsx
@@ -1,4 +1,12 @@
-import { cn, formatPrice } from "@/shared/lib/utils";
+import { useShallow } from "zustand/react/shallow";
+import { OrderListItem, useOrderStore } from "@/entities/order";
+import { useTableOrder } from "@/entities/table";
+import { type ActiveOrderProps } from "../model/types";
+import { formatPrice } from "@/shared/lib/utils";
+
+import { ChangeOrderItemQuantity } from "@/features/change-order-item-quantity";
+import { RemoveOrderItemButton } from "@/features/remove-order-item-button";
+import { SendToKitchenButton } from "@/features/send-to-kitchen-button";
 
 import {
   Card,
@@ -8,38 +16,64 @@ import {
   CardFooter,
 } from "@/shared/ui/card";
 import { ScrollArea } from "@/shared/ui/scroll-area";
+import { Badge } from "@/shared/ui/badge";
 
-import { OrderListItem } from "@/entities/order";
 import { EmptyState } from "./EmptyState";
+import { ActiveOrderSkeleton } from "./ActiveOrderSkeleton";
 
-import { useOrderStore } from "@/features/manage-order";
+export function ActiveOrder({ tableId }: ActiveOrderProps) {
+  const { isLoading } = useTableOrder(tableId);
 
-export function ActiveOrder() {
-  const orderItems = useOrderStore((store) => store.orderItems);
-  const totalAmount = useOrderStore((store) => store.totalAmount);
+  const { orderStatus, orderItems, orderTotalAmount } = useOrderStore(
+    useShallow((state) => ({
+      orderStatus: state.currentOrder?.status,
+      orderItems: state.currentOrder?.items,
+      orderTotalAmount: state.currentOrder?.totalAmount,
+    })),
+  );
+
+  if (isLoading) {
+    return <ActiveOrderSkeleton />;
+  }
 
   return (
     <Card className="flex-1 p-0 overflow-hidden">
       <CardHeader className="p-2 gap-0 bg-gray-50 border-b">
-        <CardTitle>Current Order</CardTitle>
+        <CardTitle className="flex justify-between items-center">
+          <span>Current Order</span>
+          <Badge className="bg-green-200 text-green-700 dark:bg-green-950 dark:text-green-300">
+            {orderStatus}
+          </Badge>
+        </CardTitle>
       </CardHeader>
-      <CardContent className="flex-1 tex-sm px-4">
+      <CardContent className="flex-1 text-sm px-4">
         {!orderItems?.length ? (
           <EmptyState />
         ) : (
-          <ScrollArea className="flex-1 h-full w-full">
-            {orderItems.map((oderItem) => (
-              <OrderListItem
-                key={oderItem.productId}
-                orderItemData={oderItem.product}
-                quantity={oderItem.quantity}
-              />
-            ))}
-          </ScrollArea>
+          <>
+            <ScrollArea className="flex-1 h-full w-full">
+              {orderItems.map(({ productId, product, quantity }) => (
+                <OrderListItem
+                  key={productId}
+                  orderItemData={product}
+                  quantityAction={
+                    <ChangeOrderItemQuantity
+                      productId={productId}
+                      quantity={quantity}
+                    />
+                  }
+                  removeAction={<RemoveOrderItemButton productId={productId} />}
+                />
+              ))}
+            </ScrollArea>
+            <div className="flex justify-end">
+              <p>Total: {formatPrice(orderTotalAmount)}</p>
+            </div>
+          </>
         )}
       </CardContent>
-      <CardFooter className="p-2 bg-gray-50 border-t">
-        <p>Total: {formatPrice(totalAmount)}</p>
+      <CardFooter className="bg-gray-50 border-t px-0">
+        <SendToKitchenButton tableId={tableId} />
       </CardFooter>
     </Card>
   );
